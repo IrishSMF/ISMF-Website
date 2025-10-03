c61a01d4be4cbccf227d6e735237e83932401c4b
lybytes <lybi@tcd.ie>
2025-10-03 17:04:15 +0100

    Fixed library installations so app can run

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Home(){
+    return (
+        <div className="home-container">
+            <h1>Welcome to the Irish Student Managed Fund!</h1>
+        </div>
+        
+    )
+}
