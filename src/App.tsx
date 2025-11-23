9e18c24219c526e9dfdf4b186c4121e222e817c9
Raj Aryan Upadhyaya <upadhyra@tcd.ie>
2025-11-23 21:52:13 +0000

    Made a tiny change, hope it should fix

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/ISMF-Website">
+        <BrowserRouter basename="/">
           <Routes>
             <Route path="/" element={<Layout />}>
               <Route index element={<Index />} />
